4292e9e8560f98c101b0af83e1ccef401e8b0a31
Waizul <waizul@gmail.com>
2024-09-25 19:35:39 +0600

    Responsive navbar.

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -7,7 +7,7 @@ type PropsType = {
 
 const MaxWidthWrapper = ({ className, children }: PropsType) => {
   return (
-    <div className={cn("px-2 md:px-4 lg:px-8 w-full max-w-screen-2xl mx-auto")}>
+    <div className={cn("px-2 md:px-4 lg:px-8 w-full max-w-screen-2xl mx-auto", className)}>
       {children}
     </div>
   );
